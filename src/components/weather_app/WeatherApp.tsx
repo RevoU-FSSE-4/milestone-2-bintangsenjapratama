10499bd45b6e0f4cfb7cc178eed4bf09425aed5b
Bintang S Pratama <bintangsenjapratama@gmail.com>
2024-05-08 11:09:34 +0800

    fix: comment unused variables

--- src/components/weather_app/WeatherApp.tsx
+++ src/components/weather_app/WeatherApp.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./WeatherApp.css";
 
 import search_icon from "../assets/search.png";
-import clear_icon from "../assets/clear.png";
+// import clear_icon from "../assets/clear.png";
 import cloud_icon from "../assets/cloud.png";
-import drizzle_icon from "../assets/drizzle.png";
-import rain_icon from "../assets/rain.png";
-import snow_icon from "../assets/snow.png";
+// import drizzle_icon from "../assets/drizzle.png";
+// import rain_icon from "../assets/rain.png";
+// import snow_icon from "../assets/snow.png";
 import wind_icon from "../assets/wind.png";
 import humidity_icon from "../assets/humidity.png";
 
